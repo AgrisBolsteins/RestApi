01b50b4907b3e903f40a53dfa892b84fcb72229b
Agris Bolšteins <agris.bolsteins@testdevlab.com>
2018-06-20 18:22:47 +0300

    zina

---
+++ reporter.rb
@@ -0,0 +1,25 @@
+require_relative 'features/support/api_helper'
+require 'json'
+
+thumbnail = {'url' => 'https://pbs.twimg.com/profile_images/662215297378574336/YOOD0ijr_400x400.png'}
+
+fields = []
+
+fields.push({'name' => 'Authors', 'value' => 'Agris'})
+fields.push({'name' => 'Mērķis', 'value' => 'Rest kurss'})
+
+embed = []
+embed.push('title' =>  'Test title',
+           'colour' => 16007746,
+           'thumbnail' => thumbnail,
+           'fields' => fields)
+
+payload = {
+    'content' => 'Agris Bolsteins',
+    'embeds' => embed
+}.to_json
+
+response = API.post('https://discordapp.com/api/webhooks/459005793627406359/-6FGK6la-XlTO_UZ2G62Cx-TZ0AwmMqIPd3rBkusV1nwPQo1RlmhAp3Kaxn_gHS85Aqs',
+         headers: {'Content-Type' => 'application/json'},
+         cookies: {},
+         payload: payload)
