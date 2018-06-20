70d9aeee5117ec78320e7e0b0ff8a09a47e806bf
Agris Bolšteins <agris.bolsteins@testdevlab.com>
2018-06-20 18:29:54 +0300

    job

--- reporter.rb
+++ reporter.rb
@@ -1,12 +1,17 @@
 require_relative 'features/support/api_helper'
 require 'json'
 
+job_name = ARGV[0]
+job_number = ARGV[1]
+
 thumbnail = {'url' => 'https://pbs.twimg.com/profile_images/662215297378574336/YOOD0ijr_400x400.png'}
 
 fields = []
 
 fields.push({'name' => 'Authors', 'value' => 'Agris'})
 fields.push({'name' => 'Mērķis', 'value' => 'Rest kurss'})
+fields.push({'name' => 'Job', 'value' => job_name})
+fields.push({'name' => 'Build number', 'value' => job_number})
 
 embed = []
 embed.push('title' =>  'Test title',
